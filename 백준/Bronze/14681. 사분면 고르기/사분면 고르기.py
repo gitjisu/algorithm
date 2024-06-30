88003dc848c0aab390089c785d1d6d04bb073e4a
KIMJISU <kimvely0422@gmail.com>
2024-06-30 17:08:30 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.read().strip()
+
+x, y = map(int, input.split())
+
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)
+elif x < 0 and y < 0:
+    print(3)
+elif x > 0 and y < 0:
+    print(4)
