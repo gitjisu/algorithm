7fdbc100106fdab255257fc2972e67b4f5a40ac8
KIMJISU <kimvely0422@gmail.com>
2025-01-26 17:50:34 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 640 ms, Memory: 67176 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,31 +1,33 @@
 import sys
+
 from collections import defaultdict
-
 sys.setrecursionlimit(10**7)
+def dfs(v) :
+  visited[v] = True
+  
+  for i in graph[v] :
+    if not visited[i] :
+      dfs(i)
+ 
+
 input = sys.stdin.readline
-
 n, m = map(int, input().split())
-
 graph = [[] for _ in range(n+1)]
-for _ in range(m):
-    s, e = map(int, input().split())
-    graph[s].append(e)
-    graph[e].append(s)
+
+
+for _ in range(m) :
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+  
+
+answer = 0
+visited = [False] * (n+1)
+for i in range(1, n+1) :
+  if not visited[i] :
+    dfs(i)
+    answer += 1
+
+print(answer)
     
 
-visited = [False] * (n+1)
-
-def dfs(v):
-    visited[v] = True
-    for i in graph[v]:
-        if visited[i] == False:
-            visited[i] = True
-            dfs(i)
-
-count = 0
-for i in range(1, n+1):
-    if visited[i] == False:
-        count += 1
-        dfs(i)
-        
-print(count)
